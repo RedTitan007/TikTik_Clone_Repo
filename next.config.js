adf28e32f18d9604b55c2a8600b3775c5a4ead59
Sujith Madhyastha <sujith.madhyastha1@outlook.com>
2022-11-27 11:59:04 +0530

    Steps and Config Files Changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
-  images:{
-    domains:["www.shutterstock.com","lh3.googleusercontent.com"]
-  }
-}
+  images: {
+    domains: ["www.shutterstock.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
